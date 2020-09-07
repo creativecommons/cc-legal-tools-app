0865cb23a90832e3a8ef13d5407ada5d57761412
Christopher Dixon <cdixon@caktusgroup.com>
2020-09-07 16:10:29 -0400

    lint templatetags

--- licenses/templatetags/lang_pad_horizontal.py
+++ licenses/templatetags/lang_pad_horizontal.py
@@ -2,12 +2,13 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag
 def lang_pad_horizontal(is_rtl, is_reverse):
     """Adjust padding left or right based on language direction
 
     Parameter(s):
-        - is_rtl - (boolean) whether or not the langauge of the 
+        - is_rtl - (boolean) whether or not the langauge of the
                  - of the template is a right to left language
     """
     if not is_reverse:
