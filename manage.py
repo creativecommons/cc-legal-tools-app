01ab21f0c528469988dc80913efbdbbca02cfb25
Pratik Daigavane <daigavanep@gmail.com>
2021-01-12 23:24:24 +0530

    added dontenv and fix the location of settings file in manage.py

--- manage.py
+++ manage.py
@@ -8,11 +8,9 @@ if __name__ == "__main__":
     dotenv.read_dotenv()
     if "DATABASE_URL" in os.environ:
         # Dokku or similar
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                              "cc_licenses.settings.deploy")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cc_licenses.settings.deploy")
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                              "cc_licenses.settings.local")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cc_licenses.settings.local")
 
     from django.core.management import execute_from_command_line
 
