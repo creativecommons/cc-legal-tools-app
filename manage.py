197921fe8c30d4870946a972e441dbc2f3e7f36a
Timid Robot Zehta <timid@creativecommons.org>
2021-02-25 09:41:27 -0800

    make executable and update syntax

--- manage.py
+++ manage.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
+# Standard library
 import os
 import sys
 
 if __name__ == "__main__":
     if "DATABASE_URL" in os.environ:
         # Dokku or similar
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cc_licenses.settings.deploy")
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "cc_licenses.settings.deploy"
+        )
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cc_licenses.settings.local")
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "cc_licenses.settings.local"
+        )
 
+    # Third-party
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
