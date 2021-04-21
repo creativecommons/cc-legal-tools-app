73ce4d50a8e96fde7dbfd5a1e7054a087bb23ae9
Brylie Christopher Oxley <brylie@amble.fi>
2021-04-22 00:59:19 +0300

    Add local database config

--- cc_licenses/settings/local.example.py
+++ cc_licenses/settings/local.example.py
@@ -1,5 +1,11 @@
+# Third-party
+import dj_database_url
+
 # First-party/Local
 from cc_licenses.settings.dev import *  # noqa: F401, F403
 
 # Override settings here
 # TRANSLATION_REPOSITORY_DEPLOY_KEY = "/path/to/ssh/private/key"
+
+# DATABASE_URL environment variable must be set
+DATABASES["default"] = dj_database_url.config(conn_max_age=600)  # noqa: F405
