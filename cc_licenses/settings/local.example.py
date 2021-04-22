0166b0c56c2f14cb1b57802e310600e924d000ab
Timid Robot Zehta <timid@creativecommons.org>
2021-04-22 14:46:02 -0700

    fix new variable name in comment

--- cc_licenses/settings/local.example.py
+++ cc_licenses/settings/local.example.py
@@ -10,8 +10,8 @@ from cc_licenses.settings.dev import *  # noqa: F401, F403
 # Override settings here
 # TRANSLATION_REPOSITORY_DEPLOY_KEY = "/path/to/ssh/private/key"
 
-# If DATABASE_URL environment variable is not set, then localhost is assumed.
-# Also see DATABASES variable in settings base.by.
+# If DEV_DATABASE_URL environment variable is not set, then localhost is
+# assumed.  Also see DATABASES variable in settings base.by.
 if os.getenv("DEV_DATABASE_URL"):
     DATABASES["default"] = dj_database_url.config(  # noqa: F405
         env="DEV_DATABASE_URL",
