4a24e3a591cc9433ef4e15085dcecfce90f52933
Brylie Christopher Oxley <brylie@amble.fi>
2021-04-22 21:10:31 +0300

    Switch to DEV_DATABASE_URL

--- cc_licenses/settings/local.example.py
+++ cc_licenses/settings/local.example.py
@@ -9,7 +9,8 @@ from cc_licenses.settings.dev import *  # noqa: F401, F403
 
 # If DATABASE_URL environment variable is not set, then localhost is assumed.
 # Also see DATABASES variable in settings base.by.
-if os.getenv("DATABASE_URL"):
+if os.getenv("DEV_DATABASE_URL"):
     DATABASES["default"] = dj_database_url.config(  # noqa: F405
-        conn_max_age=600
+        env="DEV_DATABASE_URL",
+        conn_max_age=600,
     )
