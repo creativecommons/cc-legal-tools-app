creativecommons/cc-legal-tools-app

commit 822a9477a31887e2939a7a26ec9b378721ab6553

WORK IN PROGRESS: begin translation rewrite
- Fix #164
  - Add settings variables and set appropriately
    - DEEDS_UX_RESOURCE_NAME = "Deeds & UX"
    - DEEDS_UX_RESOURCE_SLUG = "deeds_ux"
- Improve langauge info/support (fix #165)
  - Use Babel to populate as much info as possible
- Begin woring on #162

i18n/__init__.py
@@ -118,11 +118,11 @@ DEFAULT_JURISDICTION_LANGUAGES = {
     "pr": "es",
     "pt": "pt",
     "ro": "ro",
-    "rs": "sr",
     "scotland": "en-gb",
     "se": "sv",
     "sg": "en-gb",
     "si": "sl",
+    "sr": "sr-latn",  # because "sr" Deed & UX translation is not complete
     "th": "th",
     "tw": "zh-hant",
     "ua": "zh-hant",
@@ -187,11 +187,11 @@ JURISDICTION_NAMES = {
     "pr": "Puerto Rico",
     "pt": "Portugal",
     "ro": "Romania",
-    "rs": "Serbia",
     "scotland": "UK: Scotland",
     "se": "Sweden",
     "sg": "Singapore",
     "si": "Slovenia",
+    "sr": "Serbia",
     "th": "Thailand",
     "tw": "Taiwan",
     "ua": "Ukraine",
@@ -212,26 +212,39 @@ LANGMAP_DJANGO_TO_TRANSIFEX = {
     # Transifex language codes are ISO 639 language codes optionally followed
     # by a ISO 3166 country code or ISO 15924 script code
     # https://www.transifex.com/explore/languages/
+    #
+    # Any changes here should also be made in .tx/config
+    "de-at": "de_AT",
+    "en-ca": "en_CA",
     "en-gb": "en_GB",
+    "es-ar": "es_AR",
+    "es-pe": "es_PE",
+    "fa-ir": "fa_IR",
+    "fr-ca": "fr_CA",
+    "fr-ch": "fr_CH",
+    "oc-aranes": "oc@aranes",
     "pt-br": "pt_BR",
+    "si-lk": "si_LK",
+    "sr-latn": "sr@latin",
     "zh-hans": "zh-Hans",
     "zh-hant": "zh-Hant",
+    "zh-hk": "zh_HK",
 }
 LANGMAP_LEGACY_TO_DJANGO = {
     # Legacy language code: Django language code
     #
+    # Note that Legacy language code is first transformed by
+    # i18n.FUNCTION_NAME
+    #
     # Django language codes are lowercase Django RFC5646 language tags:
     # https://github.com/django/django/blob/main/django/conf/global_settings.py
+    "en-us": "en",
     "es-es": "es",
+    "oci": "oc-aranes",
+    "oci-es": "oc-aranes",
     "sr-cyrl": "sr",
+    "sr-latin": "sr-latn",
     "zh": "zh-hans",
     "zh-cn": "zh-hans",
     "zh-tw": "zh-hant",
-}
-FILENAME_LANGUAGE_CODES = {
-    # CC language code: language code for path to translation files
-    # (Don't ask me why... this just seems to be how it is.)
-    "en-GB": "en",
-    "zh-Hans": "zh_Hans",
-    "zh-Hant": "zh_Hant",
 }

cc_licenses/settings/base.py
@@ -7,6 +7,7 @@ import os
 # Third-party
 import colorlog  # noqa: F401
 from babel import Locale
+from babel.core import UnknownLocaleError
 from django.conf.locale import LANG_INFO
 
 APP_NAME = "licenses"
@@ -168,39 +169,63 @@ LOGGING = {
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
+DEEDS_UX_RESOURCE_NAME = "Deeds & UX"
+DEEDS_UX_RESOURCE_SLUG = "deeds_ux"
+
+# Language code for this installation.
 LANGUAGE_CODE = "en"  # "en" matches our default language code in Transifex
 
-# https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
-# Teach Django about a few more languages
-LANG_INFO["an"] = {  # Aragonese
+# Teach Django about a few more languages (sorted by langauge code):
+
+# Aragonese
+LANG_INFO["an"] = {
     "bidi": False,
     "code": "an",
     "name": "Aragonese",
     "name_local": "aragonés",
 }
-mi = Locale.parse("mi")
-LANG_INFO["mi"] = {  # Maori
+# Maori
+LANG_INFO["mi"] = {"code": "mi"}  # Remaining data from Babel
+# Malay
+LANG_INFO["ms"] = {"code": "ms"}  # Remaining data from Babel
+# Maltese
+LANG_INFO["mt"] = {"code": "mt"}  # Remaining data from Babel
+# Northern Sotho
+LANG_INFO["nso"] = {
     "bidi": False,
-    "code": "mi",
-    "name": mi.get_display_name("en"),  # in english
-    "name_local": mi.get_display_name("mi"),  # in their own language
+    "code": "nso",
+    "name": "Northern Sotho",
+    "name_local": "Sesotho sa Leboa",
 }
-LANG_INFO["ms"] = {  # Malay
+# Aranese (an Occitan variant)
+LANG_INFO["oc-aranes"] = {
     "bidi": False,
-    "code": "ms",
-    "name": "Malay",
-    "name_local": "Bahasa Melayu",  # ??
+    "code": "oc-aranes",
+    "name": "Aranese",
+    "name_local": "aranés",
 }
-LANG_INFO["oci"] = {  # Occitan? https://iso639-3.sil.org/code/oci
-    "bidi": False,
-    "code": "oci",
-    "name": "Occitan",
-    "name_local": "Occitan",
+# Sinhala (Sri Lanka)
+LANG_INFO["si-lk"] = {"code": "si-lk"}  # Remaining data from Babel
+# Zulu
+LANG_INFO["zu"] = {"code": "zu"}  # Remaining data from Babel
+
+# Normalize languages using Babel
+order_to_bidi = {
+    "left-to-right": False,
+    "right-to-left": True,
 }
+for language_code in LANG_INFO.keys():
+    babel_code = language_code.replace("-", "_")
+    try:
+        locale = Locale.parse(babel_code)
+        lang_info = LANG_INFO[language_code]
+        lang_info["name"] = locale.get_display_name("en")
+        lang_info["name_local"] = locale.get_display_name(babel_code)
+        lang_info["bidi"] = order_to_bidi[locale.character_order]
+    except UnknownLocaleError:
+        continue
 
 
 # Local time zone for this installation. Choices can be found here:

i18n/management/commands/transstats.py
@@ -47,7 +47,11 @@ def gen_statistics(input_dir, output_file):
     for locale_identifier in sorted(lang_dirs):
         trans_dir = os.path.join(input_dir, locale_identifier, "LC_MESSAGES")
         if os.path.isdir(trans_dir):
-            trans_file = os.path.join(trans_dir, "django.po")
+            trans_file = os.path.join(
+                trans_dir,
+                f"{settings.DEEDS_UX_RESOURCE_SLUG}.po",
+            )
+
             LOG.info(trans_file)
 
             # load .po file

i18n/utils.py
@@ -4,6 +4,7 @@ import re
 from contextlib import contextmanager
 
 # Third-party
+import dateutil.parser
 import polib
 from babel import Locale, UnknownLocaleError
 from django.conf import settings
@@ -15,7 +16,6 @@ from django.utils.translation.trans_real import DjangoTranslation, translation
 from i18n import (
     DEFAULT_JURISDICTION_LANGUAGES,
     DEFAULT_LANGUAGE_CODE,
-    FILENAME_LANGUAGE_CODES,
     JURISDICTION_NAMES,
     LANGMAP_DJANGO_TO_TRANSIFEX,
     LANGMAP_LEGACY_TO_DJANGO,
@@ -156,6 +156,47 @@ def get_pofile_content(pofile: polib.POFile) -> str:
     return pofile.__unicode__()
 
 
+def get_pofile_path(
+    locale_or_legalcode: str, language_code: str, resource_slug: str
+):
+    pofile_path = os.path.abspath(
+        os.path.realpath(
+            os.path.join(
+                settings.DATA_REPOSITORY_DIR,
+                locale_or_legalcode,
+                language_code,
+                "LC_MESSAGES",
+                f"{resource_slug}.po",
+            )
+        )
+    )
+    return pofile_path
+
+
+def get_pofile_creation_date(pofile_obj: polib.POFile):
+    try:
+        po_creation_date = pofile_obj.metadata["POT-Creation-Date"]
+    except KeyError:
+        return None
+    try:
+        creation_date = dateutil.parser.parse(po_creation_date)
+        return creation_date
+    except dateutil.parser._parser.ParserError:
+        return None
+
+
+def get_pofile_revision_date(pofile_obj: polib.POFile):
+    try:
+        po_revision_date = pofile_obj.metadata["PO-Revision-Date"]
+    except KeyError:
+        return None
+    try:
+        revision_date = dateutil.parser.parse(po_revision_date)
+        return revision_date
+    except dateutil.parser._parser.ParserError:
+        return None
+
+
 def map_django_to_transifex_language_code(django_language_code: str) -> str:
     """
     Given a Django language code, return a Transifex language code.
@@ -173,7 +214,6 @@ def map_django_to_transifex_language_code(django_language_code: str) -> str:
         transifex_language_code,
         transifex_language_code,
     )
-
     return transifex_language_code
 
 
@@ -200,16 +240,7 @@ def map_legacy_to_django_language_code(legacy_language_code: str) -> str:
         django_language_code,
         django_language_code,
     )
-
     return django_language_code
-
-
-def cc_to_filename_language_code(cc_language_code: str) -> str:
-    """
-    Given a CC language code, return the language code to use
-    in its gettext translation files.
-    """
-    return FILENAME_LANGUAGE_CODES.get(cc_language_code, cc_language_code)
 
 
 def get_default_language_for_jurisdiction(

licenses/apps.py
@@ -18,6 +18,7 @@ class LicensesConfig(AppConfig):
     # optional: verbose name
     verbose_name = settings.APP_VERBOSE_NAME
 
+    # Determine languages that have met or exceed display threshold
     LANGUAGES_TRANSLATED = []
     locale_dir = os.path.join(settings.DATA_REPOSITORY_DIR, "locale")
     locale_dir = os.path.abspath(os.path.realpath(locale_dir))
@@ -26,7 +27,7 @@ class LicensesConfig(AppConfig):
             locale_dir,
             language_code,
             "LC_MESSAGES",
-            "django.po",
+            f"{settings.DEEDS_UX_RESOURCE_SLUG}.po",
         )
         if not os.path.isfile(po_file):
             continue

licenses/migrations/0005_auto_20210728_1747.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             model_name="legalcode",
             name="language_code",
             field=models.CharField(
-                help_text="Django language tag (lowercase RFC5646 language"
+                help_text="Django langauge code (lowercase RFC5646 language"
                 " tag)",
                 max_length=15,
             ),
@@ -24,7 +24,7 @@ class Migration(migrations.Migration):
             model_name="translationbranch",
             name="language_code",
             field=models.CharField(
-                help_text="Django language tag (lowercase RFC5646 language"
+                help_text="Django langauge code (lowercase RFC5646 language"
                 " tag)",
                 max_length=15,
             ),

licenses/models.py
@@ -23,7 +23,6 @@ from django.utils import translation
 # First-party/Local
 from i18n import DEFAULT_LANGUAGE_CODE
 from i18n.utils import (
-    cc_to_filename_language_code,
     get_default_language_for_jurisdiction,
     get_jurisdiction_name,
     get_translation_object,
@@ -182,7 +181,7 @@ class LegalCode(models.Model):
     )
     language_code = models.CharField(
         max_length=MAX_LANGUAGE_CODE_LENGTH,
-        help_text="Django language tag (lowercase RFC5646 language tag)",
+        help_text="Django langauge code (lowercase RFC5646 language tag)",
     )
     html_file = models.CharField(
         "HTML file",
@@ -371,22 +370,21 @@ class LegalCode(models.Model):
             content = f.read()
         return polib.pofile(content.decode(), encoding="utf-8")
 
-    def get_english_pofile(self) -> polib.POFile:
+    def get_english_pofile_path(self) -> str:
         if self.language_code != DEFAULT_LANGUAGE_CODE:
             # Same license, just in English translation:
             english_legal_code = self.license.get_legal_code_for_language_code(
                 DEFAULT_LANGUAGE_CODE
             )
-            return english_legal_code.get_pofile()
-        return self.get_pofile()
+            return english_legal_code.translation_filename()
+        return self.translation_filename()
 
     def translation_filename(self):
         """
         Return absolute path to the .po file with this translation.
         These are in the cc-licenses-data repository, in subdirectories:
           - "legalcode/"
-          - language code (should match what Django uses, not what Transifex
-            uses)
+          - language code
           - "LC_MESSAGES/"  (Django insists on this)
           - files
 
@@ -403,7 +401,7 @@ class LegalCode(models.Model):
                 os.path.join(
                     settings.DATA_REPOSITORY_DIR,
                     "legalcode",
-                    cc_to_filename_language_code(self.language_code),
+                    self.language_code,
                     "LC_MESSAGES",
                     filename,
                 )
@@ -692,7 +690,7 @@ class TranslationBranch(models.Model):
     )
     language_code = models.CharField(
         max_length=MAX_LANGUAGE_CODE_LENGTH,
-        help_text="Django language tag (lowercase RFC5646 language tag)",
+        help_text="Django langauge code (lowercase RFC5646 language tag)",
     )
     last_transifex_update = models.DateTimeField(
         "Time when last updated on Transifex.",

commit 4061c689cd8220e42203a593319b16fd7210363b

correct error introduced in 822a9477a31887e2939a7a26ec9b378721ab6553

i18n/__init__.py
@@ -118,11 +118,11 @@ DEFAULT_JURISDICTION_LANGUAGES = {
     "pr": "es",
     "pt": "pt",
     "ro": "ro",
+    "rs": "sr-latn",  # because "sr" Deed & UX translation is not complete
     "scotland": "en-gb",
     "se": "sv",
     "sg": "en-gb",
     "si": "sl",
-    "sr": "sr-latn",  # because "sr" Deed & UX translation is not complete
     "th": "th",
     "tw": "zh-hant",
     "ua": "zh-hant",
@@ -187,11 +187,11 @@ JURISDICTION_NAMES = {
     "pr": "Puerto Rico",
     "pt": "Portugal",
     "ro": "Romania",
+    "rs": "Serbia",
     "scotland": "UK: Scotland",
     "se": "Sweden",
     "sg": "Singapore",
     "si": "Slovenia",
-    "sr": "Serbia",
     "th": "Thailand",
     "tw": "Taiwan",
     "ua": "Ukraine",