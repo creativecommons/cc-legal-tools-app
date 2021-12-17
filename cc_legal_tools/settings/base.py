8650e904694182e5c3fc1beb4234cea76f8b582f
Timid Robot Zehta <timid@creativecommons.org>
2021-12-17 09:47:28 -0800

    translate list page content
    - list page: show language instead of language code
    - translate jurisdictions
    - update translation threshold

--- cc_legal_tools/settings/base.py
+++ cc_legal_tools/settings/base.py
@@ -191,7 +191,7 @@ DEEDS_UX_RESOURCE_NAME = "Deeds & UX"
 DEEDS_UX_RESOURCE_SLUG = "deeds_ux"
 
 # Percent translated that languages should be at or above
-TRANSLATION_THRESHOLD = 42
+TRANSLATION_THRESHOLD = 60
 
 DEEDS_UX_LOCALE_PATH = os.path.abspath(
     os.path.abspath(
