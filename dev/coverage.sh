0f1c1a991656672c14c29e2de72bd2c434a2ddba
Timid Robot Zehta <timid@creativecommons.org>
2024-07-14 17:27:49 -0700

    update bash script to use my common conventions

--- dev/coverage.sh
+++ dev/coverage.sh
@@ -2,38 +2,63 @@
 #
 # Run coverage tests and report
 #
-set -o errtrace
-set -o nounset
-
 # This script passes all arguments to Coverage Tests. For example, it can be
 # called from the cc-legal-tools-app directory like so:
 #
 #     ./dev/coverage.sh --failfast
 
-# Change directory to cc-legal-tools-app (grandparent directory of this script)
-cd ${0%/*}/../
+set -o errexit
+set -o errtrace
+set -o nounset
 
-if ! docker compose exec app true 2>/dev/null; then
-    echo 'The app container/services is not avaialable.' 1>&2
-    echo 'First run `docker compose up`.' 1>&2
+# shellcheck disable=SC2154
+trap '_es=${?};
+    printf "${0}: line ${LINENO}: \"${BASH_COMMAND}\"";
+    printf " exited with a status of ${_es}\n";
+    exit ${_es}' ERR
+
+DIR_REPO="$(cd -P -- "${0%/*}/.." && pwd -P)"
+# https://en.wikipedia.org/wiki/ANSI_escape_code
+E0="$(printf "\e[0m")"        # reset
+E30="$(printf "\e[30m")"      # black foreground
+E31="$(printf "\e[31m")"      # red foreground
+E107="$(printf "\e[107m")"    # bright white background
+
+#### FUNCTIONS ################################################################
+
+error_exit() {
+    # Echo error message and exit with error
+    echo -e "${E31}ERROR:${E0} ${*}" 1>&2
     exit 1
-fi
+}
 
-printf "\e[1m\e[7m %-80s\e[0m\n" 'Coverage Erase'
-docker compose exec app coverage erase
-echo 'done.'
+print_header() {
+    # Print 80 character wide black on white heading with time
+    printf "${E30}${E107}# %-69s$(date '+%T') ${E0}\n" "${@}"
+}
+
+#### MAIN #####################################################################
+
+cd "${DIR_REPO}"
+
+docker compose exec app true 2>/dev/null \
+    || error_exit \
+        'The Docker app container/service is not avaialable. See README.md'
+
+print_header 'Coverage Erase'
+docker compose exec app coverage erase --debug=dataio
 echo
 
-printf "\e[1m\e[7m %-80s\e[0m\n" 'Coverage Tests'
-docker compose exec app coverage run \
+print_header 'Coverage Tests'
+docker compose exec app coverage run --debug=pytest \
     manage.py test --noinput --parallel 4 ${@:-} \
     || exit
 echo
 
-printf "\e[1m\e[7m %-80s\e[0m\n" 'Coverage Combine'
+print_header 'Coverage Combine'
 docker compose exec app coverage combine
 echo
 
-printf "\e[1m\e[7m %-80s\e[0m\n" 'Coverage Report'
+print_header 'Coverage Report'
 docker compose exec app coverage report
 echo
