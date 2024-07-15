23b100ce8b2f3bd6d32b11e81fc24634ce74ee14
Timid Robot Zehta <timid@creativecommons.org>
2024-07-14 17:03:25 -0700

    remove quotes from variable

--- cc_legal_tools/urls.py
+++ cc_legal_tools/urls.py
@@ -48,5 +48,5 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [
-        path("__debug__/", include("debug_toolbar.urls")),
+        path("__debug__/", include(debug_toolbar.urls)),
     ]
