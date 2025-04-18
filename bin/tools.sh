5cc14f3923be67ead89dc9656a7962288a09af7c
Timid Robot Zehta <timid@creativecommons.org>
2025-04-18 18:37:35 +0200

    better align script with best and local practices

--- bin/tools.sh
+++ bin/tools.sh
@@ -1,27 +1,65 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Run Python code tools (isort, black, flake8)
 #
+#### SETUP ####################################################################
+
+set -o errexit
 set -o errtrace
 set -o nounset
 
-# Change directory to cc-legal-tools-app (grandparent directory of this script)
-cd ${0%/*}/../
+# shellcheck disable=SC2154
+trap '_es=${?};
+    printf "${0}: line ${LINENO}: \"${BASH_COMMAND}\"";
+    printf " exited with a status of ${_es}\n";
+    exit ${_es}' ERR
 
-if ! docker compose exec app true 2>/dev/null; then
-    echo 'The app container/services is not avaialable.' 1>&2
-    echo 'First run `docker compose up`.' 1>&2
+DIR_REPO="$(cd -P -- "${0%/*}/.." && pwd -P)"
+# https://en.wikipedia.org/wiki/ANSI_escape_code
+E0="$(printf "\e[0m")"        # reset
+E30="$(printf "\e[30m")"      # black foreground
+E31="$(printf "\e[31m")"      # red foreground
+E107="$(printf "\e[107m")"    # bright white background
+
+#### FUNCTIONS ################################################################
+
+check_docker() {
+    local _msg
+    if ! docker compose exec app true 2>/dev/null; then
+        _msg='The app container/services is not avaialable.'
+        _msg="${_msg}\n       First run \`docker compose up\`."
+        error_exit "${_msg}"
+    fi
+}
+
+error_exit() {
+    # Echo error message and exit with error
+    echo -e "${E31}ERROR:${E0} ${*}" 1>&2
     exit 1
-fi
+}
 
-printf "\e[1m\e[7m %-80s\e[0m\n" 'isort'
+print_header() {
+    # Print 80 character wide black on white heading with time
+    printf "${E30}${E107}# %-69s$(date '+%T') ${E0}\n" "${@}"
+}
+
+#### MAIN #####################################################################
+
+cd "${DIR_REPO}"
+
+check_docker
+
+print_header 'isort'
+# shellcheck disable=SC2068
 docker compose exec app isort ${@:-.}
 echo
 
-printf "\e[1m\e[7m %-80s\e[0m\n" 'black'
+print_header 'black'
+# shellcheck disable=SC2068
 docker compose exec app black ${@:-.}
 echo
 
-printf "\e[1m\e[7m %-80s\e[0m\n" 'flake8'
+print_header 'flake8'
+# shellcheck disable=SC2068
 docker compose exec app flake8 ${@:-.}
 echo
