59bec36c272dd3a17ec7757bebc138cd6f2e163b
Timid Robot Zehta <timid@creativecommons.org>
2025-04-18 13:04:46 +0200

    better align script with best and local practices

--- bin/dump_data.sh
+++ bin/dump_data.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Dump Django application data
 #
+#### SETUP ####################################################################
+
 set -o errexit
 set -o errtrace
 set -o nounset
@@ -12,6 +14,8 @@ trap '_es=${?};
     printf " exited with a status of ${_es}\n";
     exit ${_es}' ERR
 
+DATA_FILE='../cc-legal-tools-data/config/app_data.yaml'
+DIR_REPO="$(cd -P -- "${0%/*}/.." && pwd -P)"
 # https://en.wikipedia.org/wiki/ANSI_escape_code
 E0="$(printf "\e[0m")"        # reset
 E30="$(printf "\e[30m")"      # black foreground
@@ -19,7 +23,6 @@ E31="$(printf "\e[31m")"      # red foreground
 E107="$(printf "\e[107m")"    # bright white background
 
 #### FUNCTIONS ################################################################
-
 
 check_docker() {
     local _msg
@@ -30,29 +33,27 @@ check_docker() {
     fi
 }
 
-
 error_exit() {
     # Echo error message and exit with error
     echo -e "${E31}ERROR:${E0} ${*}" 1>&2
     exit 1
 }
 
-
 print_header() {
     # Print 80 character wide black on white heading with time
     printf "${E30}${E107}# %-70s$(date '+%T') ${E0}\n" "${@}"
 }
 
-
 #### MAIN #####################################################################
 
+cd "${DIR_REPO}"
+
 check_docker
-print_header 'Export data (LegalCode and Tool models)'
-data_file='../cc-legal-tools-data/config/app_data.yaml'
+print_header 'Django dumpdata - Export LegalCode and Tool models'
 docker compose exec app ./manage.py dumpdata \
     --format yaml \
     --indent 2 \
-    --output "${data_file}" \
+    --output "${DATA_FILE}" \
     legal_tools.LegalCode legal_tools.Tool
-du -h "${data_file}"
+du -h "${DATA_FILE}"
 echo
