e154e42b3024bd61855defb52c378096cae17818
Timid Robot Zehta <timid@creativecommons.org>
2025-11-19 22:34:35 +0100

    reduce size limit, add documentation, format using prettier

--- prettier-server.js
+++ prettier-server.js
@@ -3,14 +3,20 @@ const prettier = require("prettier");
 
 const app = express();
 
-app.use(express.text({ limit: "10mb", type: "*/*" }));
+// https://expressjs.com/en/5x/api.html#express.text
+//
+// As of 2025-11-19, the largest file in cc-legal-tools-data/docs is 7.5K
+app.use(express.text({ limit: "2mb", type: "*/*" }));
 
 app.post("/", async (req, res) => {
   try {
     const formatted = await prettier.format(req.body, { parser: "html" });
     res.type("text/html").send(formatted);
   } catch (error) {
-    res.status(500).type("text/plain").send(`Prettier error: ${error.message}`);
+    res
+      .status(500)
+      .type("text/plain")
+      .send(`Prettier error:\n${error.message}`);
   }
 });
 
