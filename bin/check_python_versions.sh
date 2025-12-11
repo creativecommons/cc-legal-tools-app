31936582db758a5f3fa5c0242cf6a1e3e8444731
Timid Robot Zehta <timid@creativecommons.org>
2025-12-11 09:49:47 +0100

    improve error handling

--- bin/check_python_versions.sh
+++ bin/check_python_versions.sh
@@ -26,34 +26,38 @@ EXIT_STATUS=0
 
 #### FUNCTIONS ################################################################
 
-pyvercompare() {
+compare_ver() {
     local _path=${1}
     local _match=${2}
     local _field=${3}
     local _unwanted="\"',["
+    local _status
+    printf "${E97}${E100} %9s${E0} %s\n" 'File:' "${E97}${_path}${E0}"
     # extract Python version
-    _ver=$(awk "/${_match}/ {print \$${_field}}" \
-        "${_path}")
-    # clean-up Python version
-    _ver="${_ver//[${_unwanted}]/}"
-    # compare Python version
-    if [[ "${_path}" == 'Pipfile' ]]
+    if _ver=$(awk "/${_match}/ {print \$${_field}}" "${_path}" 2>&1)
     then
-        _status="✅  ${E92}authoritative Python version${E0}"
-        PIPFILE_VER=${_ver}
-    elif [[ "${_ver}" == "${PIPFILE_VER}" ]] \
-        || [[ "${_ver}" == "python${PIPFILE_VER}" ]] \
-        || [[ "${_ver}" == "python:${PIPFILE_VER}" ]] \
-        || [[ "${_ver}" == "py${PIPFILE_VER//./}" ]]
-    then
-        _status="✅  ${E92}Python version matches Pipfile${E0}"
+        # clean-up Python version
+        _ver="${_ver//[${_unwanted}]/}"
+        # compare Python version
+        if [[ "${_path}" == 'Pipfile' ]]
+        then
+            _status="✅  ${E92}authoritative Python version${E0}"
+            PIPFILE_VER=${_ver}
+        elif [[ "${_ver}" == "${PIPFILE_VER}" ]] \
+            || [[ "${_ver}" == "python${PIPFILE_VER}" ]] \
+            || [[ "${_ver}" == "python:${PIPFILE_VER}" ]] \
+            || [[ "${_ver}" == "py${PIPFILE_VER//./}" ]]
+        then
+            _status="✅  ${E92}Python version matches Pipfile${E0}"
+        else
+            _status="❌  ${E91}Python version does not match Pipfile${E0}"
+            EXIT_STATUS=1
+        fi
+        printf "${E97}${E100} %9s${E0} %s\n" 'Version:' "${_ver}"
     else
-        _status="❌  ${E91}Python version does not match Pipfile${E0}"
+        _status="❌ ${E91}Unable to process file:${E0} ${_ver}"
         EXIT_STATUS=1
     fi
-    # print info
-    printf "${E97}${E100} %9s${E0} %s\n" 'File:' "${E97}${_path}${E0}"
-    printf "${E97}${E100} %9s${E0} %s\n" 'Version:' "${_ver}"
     printf "${E97}${E100} %9s${E0} %s\n" 'Status:' "${_status}"
     echo
 }
@@ -62,11 +66,11 @@ pyvercompare() {
 
 cd "${DIR_REPO}"
 
-pyvercompare 'Pipfile' 'python_version =' '3'
-pyvercompare '.github/workflows/django-app-coverage.yml' 'python-version:' '2'
-pyvercompare '.github/workflows/static-analysis.yml' 'python-version:' '2'
-pyvercompare '.pre-commit-config.yaml' 'python: python' '2'
-pyvercompare 'Dockerfile.app' 'FROM python:' '2'
-pyvercompare 'pyproject.toml' 'target-version =' '3'
+compare_ver 'Pipfile' 'python_version =' '3'
+compare_ver '.github/workflows/django-app-coverage.yml' 'python-version:' '2'
+compare_ver '.github/workflows/static-analysis.yml' 'python-version:' '2'
+compare_ver '.pre-commit-config.yaml' 'python: python' '2'
+compare_ver 'Dockerfile.app' 'FROM python:' '2'
+compare_ver 'pyproject.toml' 'target-version =' '3'
 
 exit ${EXIT_STATUS}
